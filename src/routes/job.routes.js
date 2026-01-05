fc6f1a29c75963d146c1097f008cad807bc4a1c9
Gobinda9732 <gobindajana9732@gmail.com>
2026-01-05 16:30:01 +0530

    change get all authorige

--- src/routes/job.routes.js
+++ src/routes/job.routes.js
@@ -5,7 +5,7 @@ import { verifyToken } from "../utils/jwtVerification.js";
 const JobRoutes = express.Router();
 
 
-JobRoutes.get("/get-all-job", verifyToken, getAllJob)
+JobRoutes.get("/get-all-job", getAllJob)
 JobRoutes.get("/get-job/:id", getJobUsingId)
 JobRoutes.get("/get-job-using-pagination", getJobUsingPagination);
 JobRoutes.post("/create-job", verifyToken, createJob);
